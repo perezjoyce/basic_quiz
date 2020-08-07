e5e314c13842544bf791510d7c824603f45b71ec
unknown <japerez.ph@gmail.com>
2020-08-07 17:16:32 +0800

    stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return MyAppState();
-    // throw UnimplementedError();
   }
 }
 
